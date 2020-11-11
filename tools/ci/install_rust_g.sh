ade93effadd0c8e81b359bb1c9c6de4ae94919c0
Unknown <epicmail709@yahoo.com>
2020-11-11 17:08:12 -0600

    I'm dumb

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 source buildByond.conf
 
 wget -O ./librust_g.so "https://github.com/BeeStation/rust-g/releases/download/$RUST_G_VERSION/full_librust_g.so"
-mv ./full_librust_g.so ./librust_g.so
 chmod +x ./librust_g.so
 ldd ./librust_g.so
